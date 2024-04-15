6c3276b4083f1eb118848e2bf3a83ce54d1b4103
Le Phuoc Hung <19dh111215@st.huflit.edu.vn>
2024-04-15 13:16:46 +0700

    Static files and SCSS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+const path = require("path");
 const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Template Engine
+app.engine(".hbs", engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render("home");
 });
 
 app.listen(port, () => {
